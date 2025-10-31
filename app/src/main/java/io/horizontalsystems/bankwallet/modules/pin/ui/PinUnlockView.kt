6e8ee9b229e1cc6925e476db05c9d625dcbb7d2d
Hoang Hiep <hoanghiep.hui@gmail.com>
2025-10-31 20:31:27 +0700

    fix bug UI
    Signed-off-by: Hoang Hiep <hoanghiep.hui@gmail.com>

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/ui/PinUnlockView.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/ui/PinUnlockView.kt
@@ -11,7 +11,7 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.material.Scaffold
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -22,6 +22,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -82,10 +83,7 @@ fun PinUnlock(
     }
 
     Scaffold(
-        backgroundColor = ComposeAppTheme.colors.tyler,
-        modifier = Modifier
-            .statusBarsPadding()
-            .navigationBarsPadding(),
+        modifier = Modifier,
         topBar = {
             Row(
                 modifier = Modifier
